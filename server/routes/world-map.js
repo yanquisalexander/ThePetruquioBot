b11544f98ff41b4826f54d67ebf5f75177728910
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-07-14 11:19:45 -0300

    DEV: Handle errors

--- server/routes/world-map.js
+++ server/routes/world-map.js
@@ -1,10 +1,31 @@
 import { Router } from "express";
 import WorldMap from "../../app/models/WorldMap.js";
+import Channel from "../../app/models/Channel.js";
 
 const WorldMapRouter = Router();
 
-WorldMapRouter.get("/:channel_name", (req, res, next) => {
+WorldMapRouter.get("/:channel_name", async (req, res, next) => {
     let channelName = req.params.channel_name || null;
+    const channel = await Channel.getChannelByName(channelName);
+    if (!channel) {
+        return res.status(404).json({
+            errors: [
+                "Channel not found"
+            ],
+            error_type: "channel_not_found"
+        });
+    }
+
+
+    if(channel.settings.enable_community_map.value === false) {
+        return res.status(404).json({
+            errors: [
+                "Community map is disabled"
+            ],
+            error_type: "community_map_disabled"
+        });
+    }
+
     if (channelName) {
         WorldMap.getChannelMap(channelName).then(worldMap => {
             res.json(worldMap);
