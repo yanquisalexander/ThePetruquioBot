3fb7d2878d384f8df025bdfe5a5ded7a86f6ae29
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-10-27 20:28:57 -0300

    Update Commands.controller.ts

--- app/controllers/Commands.controller.ts
+++ app/controllers/Commands.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import User from "../models/User.model";
 import { ExpressUser } from "../interfaces/ExpressUser.interface";
 import { Command, CommandPermission } from "../models/Command.model";
+import Session from "../models/Session.model";
 
 class CommandsController {
     constructor() {
@@ -13,6 +14,34 @@ class CommandsController {
         
         if(!currentUser) {
             return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const session = await Session.findBySessionId(currentUser.session.sessionId);
+
+        if(!session) {
+            return res.status(404).json({ error: 'Session not found' });
+        }
+
+        if(session.impersonatedUserId) {
+            const impersonatedUser = await User.findByTwitchId(session.impersonatedUserId);
+
+            if(!impersonatedUser) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+
+            const impersonatedChannel = await impersonatedUser.getChannel();
+
+            if(!impersonatedChannel) {
+                return res.status(404).json({ error: 'Channel not found' });
+            }
+
+            const commands = await impersonatedChannel.getCommands();
+
+            return res.json({
+                data: {
+                    commands,
+                },
+            });
         }
 
         const user = await User.findByTwitchId(parseInt(currentUser.twitchId));
@@ -42,6 +71,61 @@ class CommandsController {
         if(!currentUser) {
             return res.status(401).json({ error: 'Unauthorized' });
         }
+
+        const session = await Session.findBySessionId(currentUser.session.sessionId);
+
+        if(!session) {
+            return res.status(404).json({ error: 'Session not found' });
+        }
+
+        if(session.impersonatedUserId) {
+            const impersonatedUser = await User.findByTwitchId(session.impersonatedUserId);
+
+            if(!impersonatedUser) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+
+            const impersonatedChannel = await impersonatedUser.getChannel();
+
+            if(!impersonatedChannel) {
+                return res.status(404).json({ error: 'Channel not found' });
+            }
+
+            const { commandId } = req.params as unknown as { commandId: number };
+
+            const { response, name } = req.body;
+
+            if(!commandId || !response) {
+                return res.status(400).json({ error: 'Command and response are required' });
+            }
+
+            const command = await Command.findByIdAndChannel(commandId, impersonatedChannel);
+
+            console.log(command);
+
+            if(!command) {
+                return res.status(404).json({ error: 'Command not found' });
+            }
+
+            if(name) {
+                command.name = name.toLowerCase().replace('!', '').replace(/ /g, '-');
+            }
+
+            if(response) {
+                command.response = response;
+            }
+
+            await command.save(impersonatedChannel);
+
+            return res.json({
+                success: true,
+                data: {
+                    command,
+                    response,
+                },
+            });
+        }
+
 
         const user = await User.findByTwitchId(parseInt(currentUser.twitchId));
 
@@ -97,6 +181,14 @@ class CommandsController {
             return res.status(401).json({ error: 'Unauthorized' });
         }
 
+        const session = await Session.findBySessionId(currentUser.session.sessionId);
+
+        if(!session) {
+            return res.status(404).json({ error: 'Session not found' });
+        }
+
+
+
         const user = await User.findByTwitchId(parseInt(currentUser.twitchId));
 
         if(!user) {
@@ -133,6 +225,50 @@ class CommandsController {
         
         if(!currentUser) {
             return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const session = await Session.findBySessionId(currentUser.session.sessionId);
+
+        if(!session) {
+            return res.status(404).json({ error: 'Session not found' });
+        }
+
+        if(session.impersonatedUserId) {
+            const impersonatedUser = await User.findByTwitchId(session.impersonatedUserId);
+
+            if(!impersonatedUser) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+
+            const impersonatedChannel = await impersonatedUser.getChannel();
+
+            if(!impersonatedChannel) {
+                return res.status(404).json({ error: 'Channel not found' });
+            }
+
+            const { name, response } = req.body;
+
+            if(!name || !response) {
+                return res.status(400).json({ error: 'Command and response are required' });
+            }
+
+            const command = await Command.find(impersonatedChannel, name);
+
+            if(command) {
+                return res.status(400).json({ error: 'Command already exists' });
+            }
+
+            const newCommand = new Command(name.toLowerCase().replace('!', '').replace(/ /g, '-'), impersonatedChannel.user.username, [ CommandPermission.VIEWER ], '', {}, response);
+
+            await newCommand.save(impersonatedChannel);
+
+            return res.json({
+                success: true,
+                data: {
+                    command: newCommand,
+                    response,
+                },
+            });
         }
 
         const user = await User.findByTwitchId(parseInt(currentUser.twitchId));
