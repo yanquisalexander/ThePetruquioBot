b1f3015e2132840aa42928314135a2a50e746a00
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-11-08 18:59:28 -0300

    Update build.ts

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,19 +4,23 @@ import * as esbuild from 'esbuild';
 const rootDir = __dirname + '/..';
 
 async function build() {
-  await esbuild.build({
-    entryPoints: [rootDir + '/index.ts'],
-    bundle: true,
-    outfile: rootDir + '/dist/index.js',
-    minify: true,
-    platform: 'node',
-    plugins: [ImportGlobPlugin()],
-    packages: 'external',
-    target: ['node16']
-  });
-  
+  try {
+    await esbuild.build({
+      logLevel: 'debug',
+      entryPoints: [rootDir + '/index.ts'],
+      bundle: true,
+      outfile: rootDir + '/dist/index.js',
+      minify: true,
+      platform: 'node',
+      plugins: [ImportGlobPlugin()],
+      packages: 'external',
+      target: ['node16']
+    });
 
-  console.log('Build complete!');
+    console.log('Build complete!'); // Agrega un mensaje de registro
+  } catch (error) {
+    console.error('Build failed:', error); // Manejo de errores en caso de que la compilación falle
+  }
 }
 
 build();
