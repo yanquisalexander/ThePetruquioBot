2bd485bc019c379a9602be7f17611a56b47fbbeb
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2023-11-12 20:27:16 -0300

    Update live-now.command.ts

--- app/commands/live-now.command.ts
+++ app/commands/live-now.command.ts
@@ -11,21 +11,26 @@ const LiveNowCommand = new Command(
     {},
     '', // System commands don't need a response
     async (_user, _args, _channel, _bot) => {
-       if(_args.length === 0) {
-            return _bot.sendMessage(_channel, `@${_user.username}, los canales en vivo son: ${MemoryVariables.getLiveChannels().map((channel) => channel.userDisplayName).join(', ')}`);
-       }
-       else {
+        if (_args.length === 0) {
+            const liveChannels = MemoryVariables.getLiveChannels();
+
+            if (liveChannels.length === 0) {
+                return _bot.sendMessage(_channel, `@${_user.username}, no hay canales en vivo en este momento :(`);
+            }
+            return _bot.sendMessage(_channel, `@${_user.username}, los canales en vivo son: ${liveChannels.map(channel => channel.userDisplayName).join(', ')}`);
+        }
+        else {
             const targetUsername = _args[0].replace('@', '').toLowerCase();
 
             const isLive = await Twitch.isChannelLive(targetUsername)
 
-            if(isLive) {
+            if (isLive) {
                 return _bot.sendMessage(_channel, `@${_user.username}, sí, ${targetUsername} está en vivo ;)`);
             }
             else {
                 return _bot.sendMessage(_channel, `@${_user.username}, parece que ${targetUsername} no está en vivo en este momento :(`);
             }
-       }
+        }
     }
 );
 
