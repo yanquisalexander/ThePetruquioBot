4271715be49a7166472094602a1f73beb63a5eef
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2024-05-09 18:29:15 -0300

    feat: Eliminar clase BetterStack y sus usos
    Este commit elimina la clase BetterStack y todas sus referencias en el código. La clase BetterStack ya no se utiliza y se ha decidido eliminarla para simplificar el código y reducir la complejidad. Esta eliminación no afecta la funcionalidad del sistema, ya que no se utilizaba en ninguna parte del código.

--- backend/lib/MonkeyPatches.ts
+++ backend/lib/MonkeyPatches.ts
@@ -1,18 +1,16 @@
-import { betterStack, createBetterStack } from '@/app/services/BetterStack'
 import MemoryVariables from './MemoryVariables'
 
 class MonkeyPatches {
-  constructor () {
+  constructor() {
     throw new Error('This class cannot be instantiated')
   }
 
-  public static apply (): void {
-    createBetterStack()
+  public static apply(): void {
     this.patchConsole()
     console.log('[MONKEY PATCHES] Monkey patches applied.')
   }
 
-  private static patchConsole (): void {
+  private static patchConsole(): void {
     /* Monkey patching console.log, console.info, console.warn, console.error, and console.debug to add timestamps */
     console.log('[MONKEY PATCHES] Patching console methods...')
     const patchConsoleMethod = (originalMethod: (...args: any[]) => void, logLevel: string) => {
@@ -23,18 +21,9 @@ class MonkeyPatches {
         originalMethod(timestamp, ...args)
         const log = args.join(' ')
         MemoryVariables.getLogs().push(`${timestamp} ${log}`)
-        betterStack.logtail.log(`${timestamp} [${logLevel}] ${log}`).catch((error) => {
-          console.error('Error logging to logtail:', error)
-        })
 
-        if (logLevel === 'ERROR') {
-          betterStack.logtail.error(log).catch((error) => {
-            console.error('Error logging error to BetterStack:', error)
-          })
-        } else if (logLevel === 'INFO') {
-          betterStack.logtail.info(log).catch((error) => {
-            console.error('Error logging info to BetterStack:', error)
-          })
+        if (MemoryVariables.getLogs().length > 100) {
+          MemoryVariables.getLogs().shift()
         }
       }
     }
